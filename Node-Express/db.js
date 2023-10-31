2799533416b48f6e24a34be46ca0c66365090ddd
ale-vargas <alexcvjunior@gmail.com>
2023-10-31 16:21:04 -0300

    update

--- Node-Express/db.js
+++ Node-Express/db.js
@@ -1,54 +1,50 @@
 const clientes = [
-    {
-        id: 1,
-        nome: "Alexandre",
-        idade: 30,
-        uf: "RS",
-    }, 
-
-    {
-        id: 2,
-        nome: "Cinthia",
-        idade: 29,
-        uf: "RS",
+    { id: 1,
+      nome: "Alexandre",
+      idade: 30,
+      uf:"RS"
+    },
+    { id: 2,
+      nome: "Cinthia",
+      idade: 29,
+      uf:"RS"
     }
-]
+];
 
-function selectClientes() {
-    return  clientes
+function selectClientes(){
+    return clientes;
 }
 
-function selectCliente(id) {
-    return clientes.find(c => c.id == id)
+function selectCliente(id){
+    return clientes.find(c => c.id == id);
 }
 
 // insert 
-function insertCliente(cliente) {
-    clientes.push(cliente)
+function insertCliente(cliente){
+    clientes.push(cliente);
 }
 
 // update
-function updateCliente(id, novoCliente) {
-    const cliente = clientes.find(c => c.id == id)
-    if (!cliente) {
-        return
+function updateCliente(id, novoCliente){
+    const cliente = clientes.find(c => c.id == id);
+    if(!cliente){
+        return;
     }
-    cliente.nome = novoCliente.nome
-    cliente.idade = novoCliente.idade
-    cliente.uf = novoCliente.uf
+    cliente.nome = novoCliente.nome;
+    cliente.idade = novoCliente.idade;
+    cliente.uf = novoCliente.uf;
 }
 
 // delete 
-function deleteCliente(id) {
-    const idx = clientes.findIndex(c => c.id == id)
-    clientes.slice(idx,1)
+function deleteCliente(id){
+    const idx = clientes.findIndex(c => c.id == id);
+    clientes.splice(idx,1);
 }
-
 
 module.exports = {
     selectClientes,
     selectCliente,
     insertCliente,
     updateCliente,
-    deleteCliente,
+    deleteCliente
 }
--- Node-Express/index.js
+++ Node-Express/index.js
@@ -1,34 +1,41 @@
-require("dotenv").config() // Gestão das configurações do projeto
-const db = require ("./db")
-const express =  require("express") // Framework web para a construção da infraestrutura da API
-const cors = require("cors") // Necessário para permitir o acesso da URL
-const app = express()
+require("dotenv").config(); // Gestão das configurações do projeto
 
-app.use(express.json())
-app.use(cors()) // App deve fazer uso do CORS
+const db = require("./db");
 
+const express = require("express"); // Framework web para a construção da infraestrutura da API
 
-// insert
-app.post("/clientes", (request, response) => {  
-    const cliente = request.body
-    db.insertCliente(cliente)
-    response.sendStatus(201)
-})
+//Necessário instalar o CORS: npm install cors --save
+const cors = require("cors"); // Necessário para permitir o acesso da URL
 
-// update
-app.patch("/clientes/:id", (request, response)  => { 
-    const id = parseInt(request.params.id)
-    const cliente = request.body
-    db.updateCliente(id, cliente)
-    response.sendStatus(200)
-})
+const app = express();
+
+app.use(express.json());
+
+//App deve fazer uso do CORS
+app.use(cors());
 
 // delete
 app.delete("/clientes/:id", (request, response) => {
-    const id = parseInt(request.params.id)
-    db.deleteCliente(id)
-    response.sendStatus(204)
+    const id = parseInt(request.params.id);
+    db.deleteCliente(id);
+    response.sendStatus(204);
 })
+
+// insert
+app.post("/clientes", (request,response) => {
+    const cliente = request.body;
+    db.insertCliente(cliente);
+    response.sendStatus(201);
+})
+
+// update
+app.patch("/clientes/:id", (request,response) => {
+    const id = parseInt(request.params.id);
+    const cliente = request.body;
+    db.updateCliente(id, cliente);
+    response.sendStatus(200);
+})
+
 
 app.get("/clientes", (request, response) => {
     //Os cabeçalhos abaixo devem ser setados
@@ -40,25 +47,18 @@ app.get("/clientes", (request, response) => {
     response.json(db.selectClientes());
 })
 
+app.get("/clientes/:id", (request, response) => {
+    const id = parseInt(request.params.id);
+    response.json(db.selectCliente(id));
+})
 
-// localhost:3000
-app.get("/", (request, response, next) => {    
-    response.json ({
-        message: "Estou vivo!" 
+app.get("/", (request, response, next) => {
+    response.json({
+        message: "Estou vivo!"
     })
 })
-app.listen(process.env.PORT, () => {
-    console.log("App está executando")
-})
 
-// localhost:3000/clientes
-app.get("/clientes", (request, response) => {    
-    response.json(db.selectClientes())
-})
 
-// localhost:3000/clientes/1
-app.get("/clientes/:id", (request, response) => {   
-    const id = parseInt(request.params.id)
-    response.json(db.selectCliente(id))
-})
-
+app.listen(process.env.PORT, () =>{
+    console.log("App está executando");
+});
